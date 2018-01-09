91f9001d6e28ec62b0588f6a8eb1421854500b79
log <giacosalorenzo89@gmail.com>
2018-01-09 16:31:12 +0100

    Added FIrebase realtime database

--- app/src/main/java/it/log/tably/MainActivity.kt
+++ app/src/main/java/it/log/tably/MainActivity.kt
@@ -3,7 +3,19 @@ package it.log.tably
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ValueEventListener
+
+
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,5 +42,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+
+        val database = FirebaseDatabase.getInstance()
+
+        val myRef = database.getReference("players")
+
+        // Read from the database
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.value
+                Log.d("asd", "Value is: " + value!!)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("asd", "Failed to read value.", error.toException())
+            }
+        })
+
+
     }
 }
